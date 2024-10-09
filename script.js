07de3feb53d988ad377778dffd43d5c5cb422ef5
boris honck <162339176+mauricedonck@users.noreply.github.com>
2024-10-09 09:54:28 +0200

    kleur_verandert

---
+++ script.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+
+function changeColortitle(){
+	document.getElementById("title").style.color="green";
+	document.getElementById("title").textContent = 
+}
+function changeColor2(){
+	document.getElementById("moo-deng").style.color="pink";
+	document.getElementById("moo-deng").textContent = "hipo";
+}
+function changeColor3(){
+	document.getElementById("pesto").style.color="orange";
+	document.getElementById("pesto").textContent = "penguin";
+}
+function changeColor4(){
+	document.getElementById("hua-hua").style.color="black";
+	document.getElementById("title").textContent =
+}
+function changeColor5(){
+	document.getElementById("harambe").style.color="gray";
+	document.getElementById("title").textContent =
+}
